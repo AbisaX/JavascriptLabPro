c2c8872bb087eb9f5f90f69fcc4a3b7325bb58c5
AbisaX <73003791+AbisaX@users.noreply.github.com>
2025-04-08 07:55:36 -0500

    an

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 function obtenerDatos() {
-
-    const nombreFormulario = document.getElementById('nombre').value;
-    const correo = document.getElementById('correo').value;
+    const nombreFormulario = document.getElementById('nombre16').value;
+    const correo = document.getElementById('correo16').value;
 
     const nombrePrompt = prompt("¿Cuál es tu nombre?");
 
     alert(`Hola ${nombrePrompt || nombreFormulario}, se ha enviado tu información.`);
 
-    const resultado = document.getElementById('resultado');
+    const resultado = document.getElementById('resultado16');
     resultado.innerHTML = `
       <strong>Nombre (formulario):</strong> ${nombreFormulario}<br>
       <strong>Nombre (prompt):</strong> ${nombrePrompt}<br>
